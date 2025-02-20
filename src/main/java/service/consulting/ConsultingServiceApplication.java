060541041746dae8de20e032dbb3140d573a0cf7
X13948 <rbenitezc@proveedor.intercorp.com.pe>
2025-02-20 04:35:31 -0500

    Se integra al eureka

--- src/main/java/service/consulting/ConsultingServiceApplication.java
+++ src/main/java/service/consulting/ConsultingServiceApplication.java
@@ -2,8 +2,10 @@ package service.consulting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsultingServiceApplication {
 
 	public static void main(String[] args) {
